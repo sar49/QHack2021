ec7bd13ef3c50aa95545810c97ba23f400a66612
meta_cube <sarveshraghu@gmail.com>
2022-01-12 22:27:05 -0600

    DONE
    Completed code

--- QML_Challenges/simple_circuits_20_template/simple_circuits_20_template.py
+++ QML_Challenges/simple_circuits_20_template/simple_circuits_20_template.py
@@ -22,10 +22,15 @@ def simple_circuits_20(angle):
     # QHACK #
 #Hi
     # Step 1 : initalize a device
-
+    num_wires = 1
+    dev = qml.device('default.qubit', wires = num_wires)
     # Step 2 : Create a quantum circuit and qnode
-
+    @qml.qnode(dev)
+    def my_quantum_function(param):
+        qml.RX(param, wires = 0)
+        return qml.probs(wires = [0])
     # Step 3 : Run the qnode
+    prob = my_quantum_function(angle)[0]
     # prob = ?
 
     # QHACK #
